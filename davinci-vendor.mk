1b399c3ed35ee994e4d08919f245db67c78c7cde
Laker <61315154+Laker87@users.noreply.github.com>
2020-03-06 13:34:37 +0300

    Update davinci-vendor.mk

--- davinci-vendor.mk
+++ davinci-vendor.mk
@@ -188,7 +188,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/davinci/proprietary/product/vendor_overlay/29/lib/liboemcrypto.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/liboemcrypto.so \
     vendor/xiaomi/davinci/proprietary/product/vendor_overlay/29/lib/modules/exfat.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/modules/exfat.ko \
     vendor/xiaomi/davinci/proprietary/product/vendor_overlay/29/lib/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/soundfx/libvolumelistener.so \
-    vendor/xiaomi/davinci/proprietary/product/vendor_overlay/29/lib64/hw/camera.qcom.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/hw/camera.qcom.so \
+#   vendor/xiaomi/davinci/proprietary/product/vendor_overlay/29/lib64/hw/camera.qcom.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/hw/camera.qcom.so \
     vendor/xiaomi/davinci/proprietary/product/vendor_overlay/29/lib64/liboemcrypto.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/liboemcrypto.so \
     vendor/xiaomi/davinci/proprietary/product/vendor_overlay/29/lib64/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/soundfx/libvolumelistener.so
 
